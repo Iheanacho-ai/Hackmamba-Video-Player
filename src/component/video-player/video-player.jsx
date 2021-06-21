0a617a99c4855dde43fd283d6a8890ed76dce4cb
Iheanacho Amarachi Sharon <58210919+Iheanacho-ai@users.noreply.github.com>
2021-06-21 05:58:46 -0700

    Added the Overlay container

--- src/component/video-player/video-player.jsx
+++ src/component/video-player/video-player.jsx
@@ -1,80 +1,69 @@
-import React, { useState } from 'react';
-import { useEffect } from 'react';
-import ReactPlayer from 'react-player';
-import VideoOverlay from '../video-overlay/video-overlay';
-import './video-player';
+import React, { useState } from "react";
+import { useEffect } from "react";
+import ReactPlayer from "react-player";
+import VideoOverlay from "../video-overlay/video-overlay";
+import "./video-player";
 
 const VideoPlayer = () => {
-    const [overlay, showOverlay] = useState(true); 
-    const [playing, setPlaying] = useState(false); 
-    const [controls, setControls] = useState(false);
+  const [overlay, showOverlay] = useState(true);
+  const [playing, setPlaying] = useState(false);
+  const [controls, setControls] = useState(false);
 
+  const onPlayMedia = () => {
+    setPlaying(true);
+  };
 
+  const onEndMedia = () => {
+    showOverlay(true);
+    setControls(false);
+  };
 
+  useEffect(() => {
+    setTimeout(() => {
+      setPlaying(true);
+    }, 3000);
+  }, []);
 
-    const onPlayMedia = () => {
-        showOverlay(false)
-        setPlaying(true);
-        setControls(true);
+  useEffect(() => {
+    setTimeout(() => {
+      showOverlay(false);
+      setControls(true);
+    }, 5000);
+  }, []);
+
+  const variants = {
+    hidden: {
+      opacity: 0
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        duration: 0.7
+      }
     }
+  };
 
-   const onEndMedia = () => {
-       showOverlay(true)
-       setControls(false);
-    }
-    
-    useEffect(() => {
-        setTimeout(() => {
-            setPlaying(true)
-        }, 3000);
-          
+  return (
+    <div className="video-player">
+      <div className="player-wrapper">
+        <ReactPlayer
+          className="react-player"
+          url="https://res.cloudinary.com/amarachi-2812/video/upload/v1622736920/production_ID_4456999_gr6iy4.mp4"
+          controls={controls}
+          pip={true}
+          playing={playing}
+          onPlay={onPlayMedia}
+          onEnded={onEndMedia}
+        />
 
-    }, [])
-    
-    useEffect(() => {
-
-        setTimeout(() => {
-            showOverlay(false)
-            setControls(true)
-
-        }, 5000);
-        
-          
-
-    }, [])
-
-
-    const variants = {
-        hidden: {
-            opacity: 0
-        },
-        visible: {
-            opacity: 1,
-            transition: {
-                duration: 0.7
-            }
-            
-        }
-    }
-    
-    return(
-        <div className="video-player">
-            <div className="player-wrapper">
-                <ReactPlayer 
-                    className = 'react-player' 
-                    url = 'https://res.cloudinary.com/amarachi-2812/video/upload/v1622736920/production_ID_4456999_gr6iy4.mp4' 
-                    controls = {true}
-                    pip= {true} 
-                    playing= {true}
-                    onPlay = {onPlayMedia}
-                    onEnded = {onEndMedia}
-                    
-                /> 
-                
-            </div>
-        </div>
-
-    )
+        <VideoOverlay
+          onPlayMedia={onPlayMedia}
+          variants={variants}
+          overlay={overlay}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default VideoPlayer;

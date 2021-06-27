890b8008bd9990a6e15369a06cd719a3f5eec586
Iheanacho Amarachi Sharon <58210919+Iheanacho-ai@users.noreply.github.com>
2021-06-26 22:45:28 -0700

    Update video-player.jsx

--- src/component/video-player/video-player.jsx
+++ src/component/video-player/video-player.jsx
@@ -35,13 +35,13 @@ const VideoPlayer = () => {
     hidden: {
       opacity: 0,
       transition: {
-        duration: 0.7
+        duration: 1
       }
     },
     visible: {
       opacity: 1,
       transition: {
-        duration: 0.7
+        duration: 1
       }
     }
   };
